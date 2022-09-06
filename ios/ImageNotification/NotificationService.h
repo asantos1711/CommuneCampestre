5a1338cc46b5f8c9a6a33f2ce6504bb88e23ce9e
Edgar Osorio <orkwizard@gmail.com>
2022-09-06 17:51:59 -0500

    ios FCM fix

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Edgar Osorio on 06/09/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
